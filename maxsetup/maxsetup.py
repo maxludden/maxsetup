802df33532d82223a6bb02a8945516094652bcd3
maxludden <dev@maxludden.com>
2022-10-30 20:19:21 -0400

    maxsetup

--- maxsetup/maxsetup.py
+++ maxsetup/maxsetup.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 from typing import Optional
+from rich import print, inspect
+from rich.console import Group
+from rich.style import Style
+from rich.panel import Panel
+from rich.table import Table, Column
+from rich.layout import Layout, LayoutRender
+from inspect import currentframe, getframeinfo
+
 
 from maxsetup.colors import (
     ANSI_COLORS,
@@ -19,7 +27,6 @@ from maxsetup.colors import (
     valid_hex,
 )
 from maxsetup.console import (
-    MaxConsole,
     console,
     progress,
     validate_console,
@@ -29,4 +36,15 @@ from maxsetup.log import log, log_init, new_run
 
 from maxsetup.myaml import load, loads, dump, dumps
 
+# Initialize Console and Progress Bar
+console = validate_console(console)
+progress = validate_progress(progress)
 
+# Initialize Logger
+current_run = new_run()
+gradient_run = gradient(f"{current_run}",3, "center")
+log = log_init(console, current_run)
+
+# Clear Console and Print Header
+console.clear()
+console.rule(title = gradient_run, style = "bold bright_white", align = 'center')
