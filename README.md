515b386f506b398cc8bcbce7cf4cf228b16f7145
maxludden <dev@maxludden.com>
2022-11-29 05:06:11 -0500

    FIxed docstring for new_run()

--- README.md
+++ README.md
@@ -6,9 +6,30 @@ Date: 2022-11-22
 CSS: static/style.css
 ...
 
-# MaxSetup v0.5.0
+# MaxSetup 0.6.0
 
-Changed setup to return only "log"
+## Version 0.6.0 Updates
+
+Updated MaxSetup so the when you call the function `new_run()` it preforms the following:
+
+- Checks to make sure that `run.txt` file exists in the <span style="color:#7700ff;">Current Working Directory (<code>CWD</code>)</span>'s log directory.
+  - If it doesn't exist, MaxSetup will:
+    -  Read the <span style="color:#7700ff;"><code>CWD</code></span>'s `pyproject.toml` and `poetry.lock` into active memory.
+    -  <span style="color:red;">Delete the files and directories in the</span> <span style="color:#7700ff;"><code>CWD</code></span><span style="color:red;">.</span>
+    -  <span style="color:#00b000;">Copy the default file structure and files from the template directory into CWD.</span>
+    -  Rewrite the `pyproject.toml` and `poetry.lock` back into the <span style="color:#7700ff;"><code>CWD</code></span>.
+- After ensuring that `run.txt` exists, it will:
+  - Read what the last run was from the file.
+  - Increment the last run to determine the current run.
+  - Write the current run to disk
+  - Setup up the loguru sinks and add the current run to the log's extra dictionary.
+  - Clear the console.
+  - Print a white horizontal rule to the console with a gradient Run title.
+  - Return the configured log and its sinks to what called it.
+
+## Changes in 0.5.0
+
+~~Changed setup to return only "log"~~ (These changes were overwritten as they caused confusion.)
 
 ## Purpose
 
@@ -42,35 +63,35 @@ python add maxsetup
 
 ## Usage
 
-<br />
-
 All you need from `maxsetup` is the following:
 
 
 ```python
-from maxsetup import setup
+from maxsetup import new_run
 
-setup()
+log = new_run()
 ```
 
 MaxSetup keeps track of the current run and logs all output to the console and to loguru sinks.
 
 In addition it creates a file structure for the project that looks like this:
 
-<pre style="background-color:#000000;border:1px solid white;border-radius: 2.5%">
-<span style="color:cyan;">.</span>
+<pre style="background-color:#000000;border:1px solid white;border-radius:1%;padding:10px;">
+<h2 style="text-align:center;font-size:2em;">Max Setup File Structure</h2>
+<span style="color:cyan;">.</span> <span style="color:cyan;font-size:.8em;">(Current working directory)</span>
+<span style="color:white;">│ </span>
 <span style="color:white;">├── .env</span>
 <span style="color:white;">├──</span> <span style="color:grey;">.gitignore</span>
-<span style="color:white;">├──</span> <span style="color:#0F7473;">.vscode</span>
+<span style="color:white;">├──</span> <span style="color:cyan;">.vscode</span>
 <span style="color:white;">│   ├──</span> <span style="color:gold;">launch.json</span>
 <span style="color:white;">│   ├──</span> <span style="color:gold;">settings.json</span>
 <span style="color:white;">│   └──</span> <span style="color:gold;">tasks.json</span>
 <span style="color:white;">├──</span> <span style="color:yellow;">LICENSE</span>
-<span style="color:white;">├──</span> <span style="color:#0F7473;">logs</span>
+<span style="color:white;">├──</span> <span style="color:cyan;">logs</span>
 <span style="color:white;">│   ├──</span> <span style="color:#00ff00;">log.log</span>
-<span style="color:white;">│   ├── run.txt</span>>
+<span style="color:white;">│   ├── run.txt</span>
 <span style="color:white;">│   └──</span> <span style="color:#00ff00;">verbose.log</span>
-<span style="color:white;">└──</span> <span style="color:#0F7473;">static</span>
+<span style="color:white;">└──</span> <span style="color:cyan;">static</span>
     <span style="color:white;">├──</span> <span style="color:orange;">Century Gothic Bold.ttf</span>
     <span style="color:white;">├──</span> <span style="color:orange;">Century Gothic.ttf</span>
     <span style="color:white;">├──</span> <span style="color:orange;">MesloLGS NF Bold Italic.ttf</span>
--- maxsetup.py
+++ maxsetup.py
@@ -2,149 +2,161 @@ from __future__ import annotations
 
 from inspect import currentframe, getframeinfo
 from pathlib import Path
+from shutil import copytree, ignore_patterns, rmtree
 from typing import Optional
-from shutil import rmtree, copytree, ignore_patterns
-
 
 from loguru import logger as log
-from rich.color import Color
-from rich.console import Console, JustifyMethod, RenderableType
-from rich.align import AlignMethod
-from rich.panel import Panel
-from rich.repr import rich_repr
-from rich.box import Box, ROUNDED
-from rich.style import Style, StyleType
-from rich.table import Column
-from rich.text import Text, TextType
-from rich.traceback import install as install_rich_traceback
-from sh import Command
-
-from maxconsole import get_theme, get_console
-from maxprogress import get_progress
+import loguru
+from loguru import _logger
 from maxcolor import gradient, gradient_panel
-import max_yaml as yaml
+from maxconsole import get_console, get_theme
+from maxprogress import get_progress
+from rich.console import Console
+from rich.traceback import install as install_rich_traceback
+
+__version__ = '0.6.0'
 
 console = get_console(get_theme())
+progress = get_progress(console)
 
 CWD = Path.cwd()
-template = Path("/Users/maxludden/dev/template")
+LOG_DIR = CWD / "logs"
+RUN = CWD / "logs" / "run.txt"
+VERBOSE_LOG = CWD / "logs" / "verbose.log"
+LOG = CWD / "logs" / "log.log"
+TEMPLATE = Path("/Users/maxludden/dev/template")
 
 # ───────────── Set Up File Structure ─────────────────────────
-@log.catch
-def _copy_files() -> None:
+
+def _create_files() -> None:
     """Copy files from the template folder to the current working directory."""
     if (CWD / "logs" / "run.txt").exists():
+
+        # Read pyproject.toml into active memory
+        TOML = CWD / 'pyproject.toml'
+        with open (TOML, 'r') as infile:
+            PYPROJECT = infile.read()
+            console.log(
+                gradient(f'Read [bold bright_white]pyproject.toml[/] into active memory.'),
+                log_locals=True
+            )
+        
+        # Read poetry.lock into active memory
+        LOCK = CWD / 'poetry.lock'
+        with open (LOCK, 'r') as infile:
+            POETRY = infile.read()
+            console.log(
+                gradient(f'Read [bold bright_white]poetry.lock[/] into active memory.'),
+                log_locals=True
+            )
+
+        # Clear current working directory    
         rmtree(CWD)
-    copytree(
-        template, CWD, ignore=ignore_patterns("*.pyc", "venv", "__pycache__", ".py")
-    )
-    console.print("")
-    console.print(
-        gradient_panel(
-            "Copied files from the template directory", title="Copied Files"
-        ),
-        justify="center",
-    )
+        console.log(
+            "[bold red]Removed the [/][bold bright_cyan]CWD[/] [bold red]file structure.[/]"
+        )
+
+        # Write pyproject.toml back into CWD
+        with open (TOML, 'w') as outfile:
+            outfile.write(PYPROJECT)
+
+        # Write poetry.lock back into CWD
+        with open (LOCK, 'w') as outfile:
+            outfile.write(POETRY)
+
+        console.log(
+            "Saved [bold bright_white]pyproject.toml[/] and [bold bright_white]poetry.lock[\] back to the [bold purple1]CWD[/]"
+        )
+
+        # Copy file structure from template directory into current directory
+        copytree(
+            TEMPLATE, CWD, ignore=ignore_patterns("*.pyc", "venv", "__pycache__", ".py")
+        )
+        console.print("")
+        console.print(
+            gradient_panel(
+                "Created file structure from template directory.", 
+                title="Created File Structure",
+                justify_text='center'
+            ),
+            justify="center",
+        )
 
 
 # ─────────────────── Run ─────────────────────────────────
-@log.catch
+
 def _get_last_run() -> int:
-    """Get the last run number from the run.txt file."""
+    """Get the last run number from the run.txt file. If the run.text file doesn't exist, re-create the entire file structure."""
+    if not RUN.exists():
+        _create_files()
     with open("logs/run.txt", "r") as infile:
         last_run = int(infile.read())
         # console.log(f"Last Run: {last_run}")
         return last_run
 
 
-@log.catch
-def _update_run(last_run: int, write: bool = True) -> int:
+
+def _increment_run(last_run: int) -> int:
     """Update the run.txt file with the next run number."""
-    run = last_run + 1
-    if write:
-        with open(Path("logs/run.txt"), "w") as outfile:
-            outfile.write(str(run))
-    return run
+    return last_run + 1
 
 
-@log.catch
-def new_run(console: Console = console) -> int:
-    """Get the next run number."""
-    if console is None:
-        console = get_console(get_theme())
 
-    # Determine the next run
-    last_run = _get_last_run()
-    run = _update_run(last_run)
-    current_run = gradient(f"Run {run}")
+def _record_run(current_run: int) -> None:
+    """Write the current run number to disk.add()
 
-    # Update the console
-    console.clear()
-    console.rule(current_run, style="bold bright_white")
+    Args:
+        current_run (`int``): The current run number.
+    """
+    with open (RUN, 'w') as run_file:
+        run_file.write(f"{current_run}")
 
-    return run
 
 
 # ──────────────────── Log Sinks  ───────────────────────────────
-BASE = Path.cwd()
-VERBOSE_LOG = BASE / "logs" / "verbose.log"
-LOG = BASE / "logs" / "log.log"
-
-
-@log.catch
-def log_init(
-    console: Optional[Console],
-    current_run: int) -> Logger:  # type: ignore
-    """
-    Configure Loguru Logger Sinks for the module.
+def setup_loguru_sinks (current_run: int, console: Console=console) -> _logger:
+    """Set up the Loguru logger and generate the logger sinks.
 
     Args:
-        `existing_console` (Optional[Console]): An existing console object to use for logging. If None, a new console will be created.add()
+        current_run (`int`): The current run number with with to bind the sinks to.
+        console (`Console`): The console with which to log to.
 
     Returns:
-        `log` (Logger): A configured Loguru Logger object.
+        log (`logger`): A Loguru logger with custom sinks.
     """
-    if console is None:
-        console = get_console(get_theme())
-    else:
-        console = console
-
     sinks = log.configure(
         handlers=[
-            # Debug - Loguru Logger
             dict(  # . debug.log
                 sink=f"{VERBOSE_LOG}",
                 level="DEBUG",
                 format="Run {extra[run]} | {time:hh:mm:ss:SSS A} | {file.name: ^13} |  Line {line: ^5} | {level: <8}ﰲ  {message}",
                 rotation="10 MB",
+
             ),
-            # INFO # Loguru Logger
-            dict(
+            dict(  # . info.log
                 sink=f"{LOG}",
                 level="INFO",
                 format="Run {extra[run]} | {time:hh:mm:ss:SSS A} | {file.name: ^13} |  Line {line: ^5} | {level: <8}ﰲ  {message}",
                 rotation="10 MB",
             ),
-            # INFO - Rich Console Log
-            dict(
-                sink=(
-                    lambda msg: console.log(
-                        msg, markup=True, highlight=True, log_locals=False
-                    )
-                ),
+            dict(  # . Rich Console Log > INFO
+                sink=(lambda msg: console.log(f"[#aaaaaa]{msg}[/]", markup=True, highlight=True, log_locals=False)),
+                level="DEBUG",
+                format="Run {extra[run]} | {time:hh:mm:ss:SSS A} | {file.name: ^13} |  Line {line: ^5} | {level: ^8} ﰲ  {message}",
+                diagnose=True,
+                catch=True,
+                backtrace=True,
+            ),
+            dict(  # . Rich Console Log > INFO
+                sink=(lambda msg: console.log(f"[#47c8ff]{msg}[/]", markup=True, highlight=True, log_locals=False)),
                 level="INFO",
                 format="Run {extra[run]} | {time:hh:mm:ss:SSS A} | {file.name: ^13} |  Line {line: ^5} | {level: ^8} ﰲ  {message}",
                 diagnose=True,
                 catch=True,
                 backtrace=True,
             ),
-            # ERROR - Rich Console Log
-            dict(
-                sink=(
-                    lambda msg: console.log(
-                        msg, markup=True, highlight=True, log_locals=True
-                    )
-                ),
+            dict(  # . Rich Console Log > ERROR
+                sink=(lambda msg: console.log(f"[bold reverse red]msg[/]", markup=True, highlight=True, log_locals=True)),
                 level="ERROR",
                 format="Run {extra[run]} | {time:hh:mm:ss:SSS A} | {file.name: ^13} |  Line {line: ^5} | {level: ^8} ﰲ  {message}",
                 diagnose=True,
@@ -152,26 +164,40 @@ def log_init(
                 backtrace=True,
             ),
         ],
-        extra={"run": current_run},  # > Current Run
+        extra={
+            "run": current_run, # > Current Run
+        },  
+        patcher=lambda record: record["extra"]
     )
-    log.debug("Initialized Logger")
+    log = log.bind(
 
+    )
     return log
 
 
-def setup() -> dict:
-    """Sets up the project by copying files from the template directory (if necessary) setting up Loguru and Rich Logging Sinks, starts a new run and prints it to the console."""
-    CWD = Path.cwd()
-    run_txt = CWD / "logs" / "run.txt"
-    if not run_txt.exists():
-        _copy_files()
-    console = get_console(get_theme())
-    progress = get_progress(console)
-    last_run = _get_last_run()
-    run = _update_run(last_run)
-    log_init(console=console, current_run=run)
-    gradient_run = gradient(f"Run {run}")
-    console.clear()
-    console.rule(gradient_run, style="bold bright_white")
+def new_run(console: Console=console) -> loguru._logger:
+    """Start a new run. Generate the file structure, log files, and `run.txt` to keep track of runs if they do not exist. Read the last run, increment it, save the new integer to disk, and then clear the console and print the current run to the console as a horizontal rule.
 
+    Args:
+        console (`Console`): The console with which to work with.
+
+    Returns:
+        log (`loguru._logger`): The Loguru Logger.
+    """
+    console.clear() # Clear the console of the activated environment sourcing.
+
+    # Retrieve, increment, and record the run number.
+    run = _increment_run(_get_last_run())
+    _record_run(run)
+
+    # Sinks
+    log = setup_loguru_sinks(run, console)
+
+    # Print the current run number to the console as a horizontal rule with gradient text.
+    console.rule(
+        title = gradient(f"Run {run}"),
+        style = "bold bright_white"
+    )
     return log
+
+    
